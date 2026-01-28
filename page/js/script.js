ccc5a3d20ca3efa85fa4c046c81a584653fda5c2
unknown <hseoglan@naver.com>
2026-01-28 16:17:17 +0900

    js 수정 끝

--- page/js/script.js
+++ page/js/script.js
@@ -3,32 +3,30 @@ const title = document.querySelector('#container h1');
 const playBtn = document.querySelector('.btn-wrap a.play');
 const stopBtn = document.querySelector('.btn-wrap a.stop');
 
-
-let play = setInterval(bgChange, 1000);
-
+let play = null; // 처음엔 멈춘 상태
 
 function getRandomColor() {
     const hex = '#' + Math.random().toString(16).slice(2, 8).padEnd(6, '0');
-    console.log(hex);
     return hex;
-};
-container.style.backgroundColor = getRandomColor();
+}
 
-
-bgChange();
 function bgChange() {
-    const ramdomColor = getRandomColor();
-    container.style.backgroundColor = ramdomColor;
-    title.textContent = ramdomColor.toUpperCase();
-};
+    const randomColor = getRandomColor();
+    container.style.backgroundColor = randomColor;
+    title.textContent = randomColor.toUpperCase();
+}
 
-stopBtn.addEventListener('click', () => {
+// Stop 버튼
+stopBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // a 태그 기본 동작 방지
     clearInterval(play);
     play = null;
 });
 
-playBtn.addEventListener('click', () => {
+// Play 버튼
+playBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     if (!play) {
         play = setInterval(bgChange, 1000);
-    };
+    }
 });
